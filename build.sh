c781d09b89cf0daa41f3696878b65ad813ff9c27
Rack Lin <racklin@gmail.com>
2015-03-27 23:07:10 +0800

    Rewritten Voice-Addons supports.
    Signed-off-by: Rack Lin <racklin@gmail.com>

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 CALLDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 PACKAGENAME="moedict-app"
-VERSION="1.1.2"
+VERSION="1.1.1"
 
 BUILDID=`date +%Y%m%d`
 DISTDIR="$CALLDIR/dist"
@@ -25,12 +25,13 @@ cp "$CALLDIR/install.rdf" "$BUILDDIR/"
 cp "$CALLDIR/LICENSE" "$BUILDDIR/"
 cp "$CALLDIR/README.md" "$BUILDDIR/"
 
-# remote manifest.appcache
+# remove manifest.appcache
 rm -f $BUILDDIR/webapp/manifest.appcache
 
 # Make sure DISTDIR exists
 if [ ! -d "$DISTDIR" ]; then mkdir -p "$DISTDIR"; fi
 
+rm -f $DISTDIR/${PACKAGENAME}-${VERSION}.xpi
 cd "$BUILDDIR" && zip -rqX "$DISTDIR/${PACKAGENAME}-${VERSION}.xpi" *
 
 rm -rf $BUILDDIR
